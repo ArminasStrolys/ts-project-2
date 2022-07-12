fdcd55ada627f4db70e9d19b925a8d365d45b3d9
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-07-12 19:48:22 +0300

    Done with variables, starting with functions

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -35,8 +35,32 @@ const logNumber: (i: number) => void = (i: number) => {
 };
 
 // When to use annotations
+
 // 1) Function that returns the 'any' type
 const json = '{"x":30, "y":65}';
 const coordinates: { x: number; y: number } = JSON.parse(json);
 // JSON.parse() returns any type
 console.log(coordinates);
+
+// 2) When we declare a variable on one line and initialize it later
+let words = ['red', 'green', 'blue'];
+// let foundWord
+// let foundWord = false
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -25, -5, 18];
+let numberAboveZero: boolean | number = false;
+// boolean or number
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
